5167e479c1c2dbe764929aeeefe172b89758fbd0
hiendv <neo.go.vn@gmail.com>
2017-11-16 17:18:54 +0700

    add comments for assertion exported functions

--- internal/test/assert.go
+++ internal/test/assert.go
@@ -4,12 +4,14 @@ import (
 	"testing"
 )
 
+// AssertOK raises a fatal error if the given error is not nil
 func AssertOK(t *testing.T, err error, assumption string) {
 	if err != nil {
 		t.Fatalf("unexpected error: %s, assumption: %s", err, assumption)
 	}
 }
 
+// AssertErr raises a fatal error if the given error is nil
 func AssertErr(t *testing.T, err error, assumption string) {
 	if err == nil {
 		t.Fatalf("unexpected success, assumption: %s", assumption)
